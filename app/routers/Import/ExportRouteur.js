167df3e3e532a756804478152f15a6e429c26764
Ewillian <guillaume.lecoq@ynov.com>
2020-03-10 16:18:21 +0100

    import data into db

---
+++ app/routers/Import/ExportRouteur.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const bodyparser = require('body-parser')
+
+const model = require('../../models/Import/ImportModel.js')
+
+module.exports = router
--- app/routers/Import/ImportRouter.js
+++ app/routers/Import/ImportRouter.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const bodyparser = require('body-parser')
+const data = require('../../models/Import/ImportModel.js')
 
 router.get('/', function(req, res, next) {
     console.log("Simple GET")
@@ -13,6 +14,17 @@ router.get('/', function(req, res, next) {
                  res.render()
              }*/
     })
+})
+
+router.post('/',(req, res, next) => {
+    console.log("POST a new data")
+    console.log(req.body)
+    data.insert(req.body).then(() => {
+        res.format({
+          //html: () => { res.redirect(`/games/`) },
+          json: () => { res.status(201).send({ code: 'ok' }) }
+        })
+    }) 
 })
 
 module.exports = router
