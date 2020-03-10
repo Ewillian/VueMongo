1b7779c72167c21a23bcf61197b0960cf4660275
Ewillian <guillaume.lecoq@ynov.com>
2020-03-10 17:00:06 +0100

    create export

---
+++ app/models/export/exportModel.js
@@ -0,0 +1,36 @@
+  
+const mongoose = require('mongoose')
+var Schema = mongoose.Schema
+let generate = require('uuid').v4()
+
+const import_schema = new Schema({ any: Schema.Types.Mixed}, {strict: false})
+
+
+let data = mongoose.model('DataBase', import_schema);
+let mongo = mongoose.connect("mongodb://localhost:27017/DataBase", {useNewUrlParser: true,useUnifiedTopology: true})
+let db = mongoose.connection
+
+module.exports = {
+
+  get: async(data_id) => {
+    var result = data.findOne({"id": data_id})
+    return await result;
+  },
+
+  insert: async(params) =>{
+    console.log("insert")
+    let newid = require('uuid').v4()
+    let collection = params;
+    JSON.stringify(collection)
+    let new_data = new data(collection)
+    console.log(new_data)
+    new_data.save()
+    .then(() => {
+      return "201"
+    })
+    .catch(err => {
+      return err
+    })
+
+  }
+} 
